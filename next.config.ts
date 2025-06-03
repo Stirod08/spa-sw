bae9a2eeff1f38e4af4cc09064e2a6e0be61068e
Stirod08 <s.rodriguez4926@gmail.com>
2025-06-02 21:22:38 -0500

    configurando archivo de next

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
-// import type { NextConfig } from "next";
+import type { NextConfig } from "next";
 
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
+const nextConfig: NextConfig = {
+  /* config options here */
+};
 
-// export default nextConfig;
-
-
-const nextConfig = {
-  output: 'export',
-}
-
-module.exports = nextConfig
-
+export default nextConfig;
