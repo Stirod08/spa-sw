a1c10175756e2f190d6035c8bb8b9dd6585dee19
Stirod08 <s.rodriguez4926@gmail.com>
2025-06-11 19:51:32 -0500

    style(components): se realizaron ajustes de estilos a los componentes de Logo, Header y Hero

--- app/ui/Inicio/Hero.tsx
+++ app/ui/Inicio/Hero.tsx
@@ -6,12 +6,12 @@ export default function Hero() {
     return (
         <>
             <section className="hero h-dvh md:h-[450px] bg-no-repeat bg-center bg-cover">
-                <section className="w-full h-full bg-black/80 flex flex-col justify-around md:justify-center items-center">
+                <section className="w-full h-full bg-black/80 flex flex-col justify-evenly items-center">
                     <div className="text-blanco text-center">
-                        <h1 className="text-4xl md:text-6xl uppercase font-black mb-4">Spaira</h1>
-                        <p className="text-3xl md:text-5xl text-orange font-bold">Respira - Renueva - Fluye.</p>
+                        <h1 className="text-6xl uppercase font-black mb-4">Spaira</h1>
+                        <p className="text-3xl md:text-5xl text-orange font-bold">Respira - Renueva - Fluye</p>
                     </div>
-                    <p className="text-blanco text-center px-4 text-xl md:max-w-3xl py-8">Bienvenido a Spaira, un espacio dedicado a reencontrar tu equilibrio interior, donde cada detalle te invita a la calma, la belleza y el bienestar integral.</p>
+                    <p className="text-blanco text-center px-4 text-xl md:max-w-3xl py-8"><strong>Bienvenid@s a <span className="uppercase">Spaira</span>,</strong> un espacio dedicado a reencontrar tu <strong>equilibrio interior,</strong> donde cada detalle te invita a <strong>la calma, la belleza y el bienestar integral.</strong></p>
 
                     <Boton hover="verdeOscuro" color="verdeClaro"  text="Conoce nuestros servicios" link="#"/>
                 </section>
--- app/ui/layout/components/Logo.tsx
+++ app/ui/layout/components/Logo.tsx
@@ -2,13 +2,13 @@ import Link from 'next/link';
 import { Shell } from 'lucide-react';
 
 export default function Logo({ scrolled }: { scrolled: boolean }) {
-    console.log(scrolled);
   return (
-    <section className={`text-2xl font-extrabold flex items-center transition-colors duration-300 ${scrolled ? 'text-verdeOscuro' : 'text-orange'}`}>
-      <Link className="flex items-center hover:scale-105 transition-transform duration-300" href="/">SPAIRA
-      <Shell className="ml-2" />
+    <section className={`text-2xl font-extrabold flex items-center transition-colors duration-300 ${scrolled ? 'text-verdeOscuro hover:text-orange' : 'text-orange hover:text-verdeOscuro'}`}>
+      <Link className="flex items-center hover:scale-105 transition-transform duration-300" href="/">
+        SPAIRA
+        <Shell className="ml-2" />
       </Link>
-      
+
     </section>
   );
 }
