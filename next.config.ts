494c9f2610a9e11d6c1a11de77d8ce916381108f
Stirod08 <s.rodriguez4926@gmail.com>
2025-06-15 13:35:07 -0500

    build: se agregan reglas de seguridad

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const securityHeaders = [
   {
     key: 'Content-Security-Policy',
     value:
-      "default-src 'self'; script-src 'self'; object-src 'none'; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob:; font-src 'self'; frame-ancestors 'none'; base-uri 'self'; form-action 'self';",
+      "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob:; font-src 'self'; frame-ancestors 'none'; base-uri 'self'; form-action 'self';",
   },
   {
     key: 'X-Frame-Options',
